d8fe36fb02ce0df808818215a411ed317a317a81
VICTORIAGUI <victoriagui.santos@gmail.com>
2023-06-02 17:07:00 -0300

    railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Kafka.wsgi --log-file -
--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-=t01a#===6*056mxge*ax_@g=-*306l=oq_g*=9)ldio!(oc=w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*"
+]
 
 
 # Application definition
@@ -117,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles/'
 STATICFILES_DIRS = [
     BASE_DIR / 'assets' / 'static'
 ]
